b3d0bd9ffdf54ce993814a226025b8a151bfb9ee
Shaohua Xiao <joexiao97@gmail.com>
2020-10-30 03:02:09 -0400

    randomized questions

--- src/components/game.js
+++ src/components/game.js
@@ -10,9 +10,44 @@ export default class TriviaGame {
             if (this.randomQuestions.indexOf(rand) === -1) this.randomQuestions.push(rand);
         }
 
-        this.score = 0;
-        this.current_question = 1;
-        console.log("hi")
+        this.curr_question;
+        this.correct_ans;
+        
+        this.start();
     };
+    
+    start(){
+        this.askQuestion();
+        this.time_limit = 30;
+        this.score = 0;
+        this.isOver = false;
+    }
+    
+    isGameOver(){
+        // if (this.isOver = true)
+    }
 
+    shuffle(a) {
+        var j, x, i;
+        for (i = a.length - 1; i > 0; i--) {
+            j = Math.floor(Math.random() * (i + 1));
+            x = a[i];
+            a[i] = a[j];
+            a[j] = x;
+        }
+        return a;
+    }
+    
+    askQuestion(){
+        let shuffled_arr = this.shuffle([0,1,2,3]);
+        this.curr_question = this.triviaQuestions[this.randomQuestions.pop()];
+        this.correct_ans = this.curr_question.correct;
+        document.getElementById("trivia-question").textContent = this.curr_question.question;
+
+        document.getElementById(`ans-choice-${(shuffled_arr.pop() + 1)}`).textContent = this.correct_ans;
+        while(shuffled_arr.length > 0){
+            let num = shuffled_arr.pop();
+            document.getElementById(`ans-choice-${(num + 1)}`).textContent = this.curr_question.incorrect.pop();
+        }
+    }
 }
